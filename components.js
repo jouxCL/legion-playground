1cc7291bc67a1254942e4bbdd445fc7dab9b6dc0
jouxCL <juansi1582@gmail.com>
2026-01-30 02:53:02 -0500

    feat: Cloud page working alpha and staffing page is now in alpha

--- components.js
+++ components.js
@@ -2,12 +2,12 @@ function loadNavbar() {
     const navbarHTML = `
     <!-- Navigation -->
     <nav class="navbar" id="navbar">
-        <div class="logo-wrap">
+        <a href="index.html" class="logo-wrap">
             <img src="assets/Legionblanco.svg" alt="Legion Logo" class="logo-icon">
-        </div>
+        </a>
         <div class="nav-right">
             <a href="index.html#home" class="nav-link">Home</a>
-            <a href="index.html#cloud" class="nav-link">Cloud</a>
+            <a href="cloud.html" class="nav-link">Cloud</a>
             <a href="staffing.html" class="nav-link">Staffing</a>
             <a href="index.html#quantum" class="nav-link">Quantum</a>
             <a href="index.html#about" class="nav-link">About us</a>
